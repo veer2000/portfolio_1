6cd141c04c7ab7342f8d7d3fc6901792c7aad0f8
veer2000 <raghupatiyal@gmail.com>
2026-01-11 01:29:41 +0530

    commented out sns file

--- backend/app/services/sns_service.py
+++ backend/app/services/sns_service.py
@@ -1,18 +1,18 @@
-import os
-import boto3
+# import os
+# import boto3
 
-AWS_REGION = os.getenv("AWS_REGION")
-ADMIN_TOPIC_ARN = os.getenv("SNS_ADMIN_TOPIC_ARN")
+# AWS_REGION = os.getenv("AWS_REGION")
+# ADMIN_TOPIC_ARN = os.getenv("SNS_ADMIN_TOPIC_ARN")
 
-sns_client = boto3.client("sns", region_name=AWS_REGION)
+# sns_client = boto3.client("sns", region_name=AWS_REGION)
 
 
-def send_admin_notification(message: str):
-    if not ADMIN_TOPIC_ARN:
-        raise ValueError("SNS_ADMIN_TOPIC_ARN is not set")
+# def send_admin_notification(message: str):
+#     if not ADMIN_TOPIC_ARN:
+#         raise ValueError("SNS_ADMIN_TOPIC_ARN is not set")
 
-    sns_client.publish(
-        TopicArn=ADMIN_TOPIC_ARN,
-        Message=message,
-        Subject="New Inquiry Received"
-    )
+#     sns_client.publish(
+#         TopicArn=ADMIN_TOPIC_ARN,
+#         Message=message,
+#         Subject="New Inquiry Received"
+#     )
